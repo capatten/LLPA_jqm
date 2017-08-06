09a9dc49d4d38bd73393afe57c2f98157badc841
capatten <pattenchada@gmail.com>
2017-08-06 11:23:22 -0600

    Styled Profile page

--- application/views/profile/profile_mobile.php
+++ application/views/profile/profile_mobile.php
@@ -2,6 +2,52 @@
 	defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
+<style>
+    div.profile-wrapper div.profile-container div.profile-title {
+        text-align: center;
+    }
+
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data {
+        text-align: center;
+    }
+
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data img {
+        width: 10em;
+        height: auto;
+        border-radius: 50%;
+    }
+
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data h2 {
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data div.profile-email h2 {
+        margin-top: 0px;
+    }
+
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data div.dept-logo {
+        height: 35px;
+    }
+
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data div.dept-logo hr {
+        margin-top: 3em;
+    }
+
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data div.dept-logo img {
+        width: 4em;
+        position: relative;
+        top: -40px;
+        background-color: white;
+    }
+
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data div.profile-fav-color,
+    div.profile-wrapper div.profile-container div.profile-details-wrapper div.profile-details-container div.profile-data div.profile-fav-animal {
+        text-align: left;
+        border-bottom: none;
+    }
+</style>
+
 <div class="profile-wrapper">
 	<div class="profile-container">
 		<div class="profile-title">
@@ -12,7 +58,7 @@
 		<div class="profile-details-wrapper">
             <div class="profile-details-container profile-details">
                 <div class="profile-data">
-                    <img src="<?php echo base_url('assets/images/') .'default.jpg'; ?>">
+                    <img src="<?php echo base_url('assets/images/') .$content['userProfile'][0]['emp_id'] .'.jpg'; ?>">
                     <div class="profile-row">
                         <div class="profile-name">
                             <h2><?php echo $content['userProfile'][0]['emp_name']; ?></h2>
@@ -24,14 +70,15 @@
                         </div>
                         <div class="dept-logo">
                             <hr/>
-                            <img src="#">
+                            <img src="<?php echo base_url('assets/images/departments/') .$_SESSION["department_desc"] .'.png'; ?>">
                         </div>
                         <div class="profile-fav-color" data-role="fieldcontain">
                             <label for="name">Favorite Color:</label>
                             <input id="name" value="[Favorite Color]" />
                         </div>
-                        <div class=" profile-fav-color">
-                            <h2>Favorite Animal:<strong>[Favorite Animal]</strong></h2>
+                        <div class="profile-fav-animal" data-role="fieldcontain">
+                            <label for="name">Favorite Animal:</label>
+                            <input id="name" value="[Favorite Animal]" />
                         </div>
                     </div>
                 </div>
